96e7c2fb4b2e6cef5f75600d91aceab59db73624
Dekagor <dekagor@hotmail.com>
2023-04-23 19:06:20 -0300

    actualizacion

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { BotonesComponent } from './components/botones/botones.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SkillComponent } from './components/skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AcercaComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    LoginComponent,
+    BotonesComponent,
+    ProyectosComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
